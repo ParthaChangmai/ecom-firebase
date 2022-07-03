bbab685c144cbd47dee0e57a8a3468022fa3c95e
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-04 02:28:55 +0530

    navbar changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,10 +9,7 @@ const Navbar = () => {
 	const { user, logOut } = useAuth();
 	const router = useRouter();
 
-	const [activeNav, setActiveNav] = useState(false);
 	const [scrollPos, setScrollPos] = useState(false);
-
-	const toggleNav = () => setActiveNav(!activeNav);
 
 	const handleLogout = () => {
 		logOut();
@@ -35,66 +32,27 @@ const Navbar = () => {
 			<div
 				className={
 					scrollPos
-						? ' min-w-screen flex justify-between bg-[#182647]/10 h-16 items-center font-bold '
-						: ' min-w-screen flex justify-between bg-[#182647]/10 h-16 items-center font-bold  shadow-md shadow-slate-300'
+						? ' min-w-screen flex justify-between text-white bg-[#182647]/10 h-16 items-center font-bold '
+						: ' min-w-screen flex justify-between text-white bg-[#182647]/10 h-16 items-center font-bold  shadow-md shadow-slate-300'
 				}
 			>
-				<div className="flex container mx-auto justify-between">
-					<div className={scrollPos ? 'invisible' : 'pl-4'}>username</div>
+				<div className="flex container mx-auto justify-end">
 					<div>
-						<ul className="hidden md:flex gap-3 pr-4 ">
-							{user && <li onClick={handleLogout}>Logout</li>}
+						<ul className="flex gap-3 pr-4 ">
+							{user && (
+								<li className="underline cursor-pointer" onClick={handleLogout}>
+									Logout
+								</li>
+							)}
 
 							<li>
-								<BsFillCartFill className="mt-1" />
+								<BsFillCartFill className="mt-1  cursor-pointer" />
+								<span className="top-4 right-12 text-xs absolute px-1 bg-blue-400 rounded-3xl">
+									2
+								</span>
 							</li>
 						</ul>
-						<div className="md:hidden pr-4 right-0">
-							{!activeNav && (
-								<FiMenu
-									onClick={toggleNav}
-									className={
-										scrollPos
-											? 'text-4xl text-white cursor-pointer p-2 bg-gray-900 rounded-full shadow-md shadow-indigo-600'
-											: 'text-4xl text-white cursor-pointer p-2 bg-transparent '
-									}
-								/>
-							)}
-						</div>
 					</div>
-				</div>
-			</div>
-
-			{/* mobile nav starts here */}
-
-			<div
-				className={
-					activeNav
-						? 'fixed flex flex-col h-screen justify-start left-0 top-0 w-full bg-gray-900  p-10 ease-in duration-500  '
-						: 'fixed flex flex-col h-screen justify-start left-[-100%] top-0  p-10 ease-in duration-500  '
-				}
-			>
-				<div className="flex justify-end ">
-					<AiOutlineClose
-						onClick={toggleNav}
-						className="bg-gray-900 p-2 text-3xl rounded-full shadow-lg shadow-indigo-600 cursor-pointer"
-					/>
-				</div>
-				<div className="pt-40">
-					<ul className="gap-5 pr-4 flex flex-col items-center">
-						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-							Home
-						</li>
-						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-							About Me
-						</li>
-						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-							Portfolio
-						</li>
-						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-							Contacts
-						</li>
-					</ul>
 				</div>
 			</div>
 		</>
