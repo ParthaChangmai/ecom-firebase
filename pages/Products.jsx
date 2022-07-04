37ae9c1b2ea77ce8a0cc19f8f8f46a3735450b6a
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-04 06:23:37 +0530

    reduced size

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import ItemCard from '../components/ItemCard';
 import product from '../data/product.json';
+import { addToCart } from '../features/cartSlice';
 
-const { id, name, url, price } = product[0];
+const { name, url, price } = product[0];
 
 const Products = () => {
-	console.log(product);
+	const dispatch = useDispatch();
+
+	const handleAddToCart = () => {
+		dispatch(addToCart(product[0]));
+	};
 	return (
 		<div>
-			<div className="bg-[#F5F5F5] mt-5 ml-4 px-2 pt-4 rounded-xl flex w-80 flex-col ">
-				<div>
-					<img src={url} className="rounded-xl" alt="" />
-				</div>
-				<div className="font-extrabold pt-2 text-3xl">{name}</div>
-				<div className="flex justify-between pt-6 pb-2">
-					<div className="text-lg p-2 font-semibold">${price}</div>
-					<div>
-						<button className="p-2 bg-blue-900 rounded-xl text-white hover:bg-blue-500 transition-all ease-in-out duration-500 ">
-							Add to cart
-						</button>
-					</div>
-				</div>
-			</div>
+			<ItemCard product={product[0]} />
 		</div>
 	);
 };
