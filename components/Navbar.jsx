3bedd1d8a0ed858b88b1c8d20159a092174e840a
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-04 06:23:01 +0530

    removed scroll shadow

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,42 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { FiMenu } from 'react-icons/fi';
+import { useSelector } from 'react-redux';
 import { BsFillCartFill, BsFillCartCheckFill } from 'react-icons/Bs';
-import { AiOutlineClose } from 'react-icons/Ai';
+
 import { useAuth } from '../context/AuthContext';
 
 const Navbar = () => {
 	const { user, logOut } = useAuth();
 	const router = useRouter();
-
-	const [scrollPos, setScrollPos] = useState(false);
+	const cart = useSelector((state) => state.cart);
 
 	const handleLogout = () => {
 		logOut();
 		router.push('/Login');
 	};
 
-	useEffect(() => {
-		const handleShadow = () => {
-			if (window.scrollY >= 90) {
-				setScrollPos(true);
-			} else {
-				setScrollPos(false);
-			}
-		};
-		window.addEventListener('scroll', handleShadow);
-	}, []);
-
 	return (
 		<>
-			<div
-				className={
-					scrollPos
-						? ' min-w-screen flex justify-between text-white bg-[#182647]/10 h-16 items-center font-bold '
-						: ' min-w-screen flex justify-between text-white bg-[#182647]/10 h-16 items-center font-bold  shadow-md shadow-slate-300'
-				}
-			>
-				<div className="flex container mx-auto justify-end">
+			<div className=" min-w-screen flex justify-between text-white bg-[#182647]/10 h-16 items-center font-bold  shadow-md shadow-slate-300">
+				<div className="flex container mx-auto justify-between">
+					<div>
+						<h1 className="pl-5">ShopCart</h1>
+					</div>
 					<div>
 						<ul className="flex gap-3 pr-4 ">
 							{user && (
@@ -45,9 +30,9 @@ const Navbar = () => {
 								</li>
 							)}
 
-							<li>
+							<li className={user ? 'flex' : 'hidden'}>
 								<BsFillCartFill className="mt-1  cursor-pointer" />
-								<span className="top-4 right-12 text-xs absolute px-1 bg-blue-400 rounded-3xl">
+								<span className="text-xs px-1 mb-2 bg-blue-400 rounded-3xl">
 									2
 								</span>
 							</li>
