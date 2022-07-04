d43bd4df3e463e8a3500bd019a691d8a5ac3caf5
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-07-04 07:19:37 +0530

    css

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
 				onSubmit={handleLogin}
 				className="flex flex-col max-w-[600px] w-full"
 			>
-				<div className="pb-8 pt-32 sm:pt-10">
+				<div className="pb-8 sm:pt-10">
 					<p className="text-4xl font-bold inline border-b-4 border-[#6133f5] text-gray-300">
 						Login
 					</p>
